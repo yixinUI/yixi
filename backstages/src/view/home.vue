c8e61ad0c258791491cd6237ebf307b99a01586b
qiwenzheng <361405342@qq.com>
2019-11-09 16:55:32 +0800

    11

--- backstages/src/view/home.vue
+++ backstages/src/view/home.vue
@@ -1,117 +1,155 @@
 <template>
 	<div class="home">
-		<div class="title">
-			<h3>
-				艺欣后台管理系统
-			</h3>
-			<div class="name">会员: 齐文蒸先生</div>
-			<p>(艺欣艺术学校管理系统)</p>
-			<div class="tui">退出</div>
-		</div>
-		<div class="box">
-			<ul class="list">
-				<li v-for="item,index in listArr" @click="qiehuan(index)" :class="curIndex == index?'active':''"><router-link :to="{path:item.src}" tag="div">{{item.title}}</router-link></li>
-			</ul>
-			<router-view class="aaa"/>
-		</div>
+		<el-container>
+			<el-header>
+				<h3>
+					艺欣后台管理系统
+				</h3>
+				<div class="name">会员: 齐文蒸先生</div>
+				<p>(艺欣艺术学校管理系统)</p>
+				<div class="tui">退出</div>
+			</el-header>
+		</el-container>
+		<el-container>
+			<el-aside width="202px">
+				<ul class="list">
+					<li v-for="item,index in listArr" @click="qiehuan(index)" :class="curIndex == index?'active':''"><router-link :to="{path:item.src}" tag="div">{{item.title}}</router-link></li>
+				</ul>
+			</el-aside>
+			<el-main><router-view class="aaa"/></el-main>
+		</el-container>
 	</div>
 </template>
 <script>
-	export default{
-		data(){
-			return{
-				curIndex:0,
-				listArr:[
-					{
-						title:'店铺图片数据管理',
-						src:'img'
-					},{
-						title:'开设课程列表数据管理',
-						src:'kecheng'
-					},{
-						title:'我的订单数据管理',
-						src:'dingdan'
-					},{
-						title:'我的预约数据管理',
-						src:'yuyue'
-					},{
-						title:'我的优惠券数据管理',
-						src:'youhui'
-					},{
-						title:'我的砍价数据管理',
-						src:'kanjia'
-					},{
-						title:'我的拼团数据管理',
-						src:'pintuan'
-					},{
-						title:'我的地址数据管理',
-						src:'address'
-					}
-				]
+	export default {
+		data() {
+			return {
+				curIndex: 0,
+				listArr: [{
+					title: '店铺图片数据管理',
+					src: 'img'
+				}, {
+					title: '开设课程列表数据管理',
+					src: 'kecheng'
+				}, {
+					title: '我的订单数据管理',
+					src: 'dingdan'
+				}, {
+					title: '我的预约数据管理',
+					src: 'yuyue'
+				}, {
+					title: '我的优惠券数据管理',
+					src: 'youhui'
+				}, {
+					title: '我的砍价数据管理',
+					src: 'kanjia'
+				}, {
+					title: '我的拼团数据管理',
+					src: 'pintuan'
+				}, {
+					title: '我的地址数据管理',
+					src: 'address'
+				}]
 			}
 		},
-		methods:{
-			qiehuan(index){
+		methods: {
+			qiehuan(index) {
 				this.curIndex = index;
 			}
 		}
 	}
 </script>
 <style scoped="" lang="scss">
-*{
-	margin:0;
-	padding:0;
-}
-.box{
-	display: flex;
-	justify-content:flex-start;
-	.aaa{
-		border:1px solid #ccc;
-		margin-top:30px;
-		margin-left:20px;
-		width:80%;
+	* {
+		margin: 0;
+		padding: 0;
 	}
-}
-.title{
-	display: flex;
-	justify-content:flex-start;
-	align-items: center;
-	background:goldenrod;
-	color:#fff;
-	height:50px;
-	line-height: 50px;
-	padding-left:50px;
-	.name{
-		margin-left:60%;
+	.home{
+		width:100%;
+		height:100vh;
 	}
-	p{
-		margin-left:1%;
-	}
-	.tui{
-		margin-left:3%;
-	}
-}
-.list{
-	display: flex;
-	justify-content: space-between;
-	align-content: center;
-	flex-flow: column;
-	margin-top:30px;
-	width:200px;
-	border:1px solid #ccc;
-	height:500px;
-	li{
-		width:200px;
-		list-style: none;
-		cursor: pointer;
-		text-align: center;
-		div{
-			padding:15px 0 15px 0px;
+	.el-container{
+		width:100%;
+		.el-header{
+			box-sizing: border-box;
+			width:100%;
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+			background: goldenrod;
+			color: #fff;
+			height: 50px;
+			line-height: 50px;
+			padding-left: 50px;
+			.name {
+				margin-left: 60%;
+			}
+			p {
+				margin-left: 1%;
+			}
+			.tui {
+				margin-left: 3%;
+			}
+		}
+		.el-aside{
+			.list {
+				justify-content: space-between;
+				align-content: center;
+				flex-flow: column;
+				margin-top: 30px;
+				width: 200px;
+				border: 1px solid #ccc;
+				height: 500px;
+				li {
+					width: 200px;
+					list-style: none;
+					cursor: pointer;
+					text-align: center;
+					div {
+						padding: 15px 0 15px 0px;
+					}
+				}
+				.active {
+					background: red;
+					color: #fff;
+				}
+			}
+		}
+		.el-main{
+			.aaa {
+				border: 1px solid #ccc;
+				margin-top: 30px;
+				margin-left: 20px;
+				width: 95%;
+			}
 		}
 	}
-	.active{
-		background: red;
-		color:#fff;
-	}
-}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 </style>
